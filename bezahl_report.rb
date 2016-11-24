fd936c52864dbb80d5ce5a051e5fa6578be6bc76
stwestphal <s.westphal@doxter.de>
2016-11-24 15:23:52 +0100

    Gemfile, Gefile.lock, .rubocop.yml, .rubocop_todo.yml, bezahl_report csv-head changes

--- bezahl_report.rb
+++ bezahl_report.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 require 'optparse'
 require 'ostruct'
 require 'csv'
@@ -6,35 +7,36 @@ require 'pry'
 # Klasse Kunde
 # speichert den Namen, Bezahltsumme, Bezahlt-zaehler, Nichtbezahlt-zaehler
 class Kunde
-  attr_accessor :summe, :name, :bezahlt, :nichtbezahlt   #ruby statt setter und getters (attr_accessor fasst attr_reader und attr_writer zusammen)
+  attr_accessor :summe, :name, :bezahlt, :nichtbezahlt
+  # ruby statt setter und getters
+  # (attr_accessor fasst attr_reader und attr_writer zusammen)
 
-  def initialize(name)    #Konstruktor in Ruby
-
+  # Konstruktor in Ruby
+  def initialize(name)
     @name = name
 
-    @bezahlt=0
-    @nichtbezahlt=0
+    @bezahlt = 0
+    @nichtbezahlt = 0
 
-    @summe=0
+    @summe = 0
   end
 
-
-  def summe=(summe)    #addiert geparste summe, vergleicht ob diese 0 ist, increment entsprechend nichtbezahlt-, oder bezahlt-zaehler
-
+  # addiert geparste summe, vergleicht ob diese 0 ist,
+  # increment entsprechend nichtbezahlt-, oder bezahlt-zaehler
+  def summe=(summe)
     @summe += summe
 
-    if summe==0
-      @nichtbezahlt+=1
+    if summe.zero?
+      @nichtbezahlt += 1
     else
-      @bezahlt+=1
+      @bezahlt += 1
     end
   end
-
 end
 
 # ----------------------------
-
-class CommandLineParser   #generiert Hilfsausgabe, fuer Aufrufe ohne parameter
+# generiert Hilfsausgabe, fuer Aufrufe ohne parameter
+class CommandLineParser
   def self.parse_command_line
     options = OpenStruct.new
 
@@ -49,68 +51,70 @@ class CommandLineParser   #generiert Hilfsausgabe, fuer Aufrufe ohne parameter
       $stderr.puts opt_parser.help
       exit(1)
     end
-    options.usage_data_filename = ARGV[0]    #speichert den Eingabeparameter in options.usage_data_filename
+    options.usage_data_filename = ARGV[0]
+    # speichert den Eingabeparameter in options.usage_data_filename
     options
   end
 end
-
 
 # Klasse Report
 # parst das Eingabe-file
 # generiert formatierte Ausgabe
 class Report
-
-  @report={}
+  @report = {}
 
   def initialize(options = {})
     @options = options
-   end
+  end
 
-  def generiere_bezahlReport(name_colum, price_colum)
+  def generiere_bezahl_report(name_colum, price_colum)
+    @gesamtsum = 0
+    ausgabe_hash = {}
 
-    @gesamtsum=0
-    ausgabeHash = {}
-
-    #puts @options.usage_data_csv
-    CSV.foreach(@options.usage_data_filename,:headers=>false) do |row|
-       #print row[1..2]
+    # puts @options.usage_data_csv
+    CSV.foreach(@options.usage_data_filename, headers: true) do |row|
+      # print row[1..2]
 
       kunden_name = row[name_colum]
       bezahl = row[price_colum].to_i
 
-      ausgabeHash[kunden_name] = Kunde.new(kunden_name) if ausgabeHash[kunden_name].nil?   #if not set: init with 0
+      if ausgabe_hash[kunden_name].nil?
+        ausgabe_hash[kunden_name] = Kunde.new(kunden_name)
+      end
+      # if not set: init with 0
 
-      ausgabeHash[kunden_name].summe = bezahl  #wir speichern unsere daten in der klasse 'kunde' in instanz-variablen
+      ausgabe_hash[kunden_name].summe = bezahl
+      # wir speichern unsere daten in der klasse 'kunde' in instanz-variablen
 
-      @gesamtsum+= bezahl
-
+      @gesamtsum += bezahl
     end
 
-    @report = ausgabeHash.drop(1)   #erste CSV-Parsezeile mit row-Benennungen wegschmeissen
+    @report = ausgabe_hash.drop(1)
+    # erste CSV-Parsezeile mit row-Benennungen wegschmeissen
   end
 
-
   def ausgabe(kundenreport, tabsigns, bezahltsigns)
-
-    printf "%-#{tabsigns}s \t%-#{bezahltsigns}s \t%-#{bezahltsigns}s \t%s\n","Kunde","Bezahlt", "Bezahlt", "Nicht Bezahlt"
-      #p @report
+    printf "%-#{tabsigns}s \t%-#{bezahltsigns}s ", 'Kunde', 'Summe'
+    printf "\t%-#{bezahltsigns}s \t%s\n", 'Bezahlt', 'Nicht Bezahlt'
+    # p @report
 
     # @report.sort do |a, b|
     #   a.last == b.last ? a.first <=> b.first :
     #   b.last <=> a.last
     # end.each do |key, value|
 
-    kundenreport.sort_by{ |kunde| kunde.to_s.downcase }.each do |_, kunde|    #to_s.downcase: kleinbuchstaben werden einsortiert
-      printf("%-#{tabsigns}s \t%-#{bezahltsigns}i \t%-#{bezahltsigns}i \t%i\n", kunde.name, kunde.summe, kunde.bezahlt, kunde.nichtbezahlt)
+    kundenreport.sort_by { |kunde| kunde.to_s.downcase }.each do |_, kunde|
+      # to_s.downcase: kleinbuchstaben werden einsortiert
+      printf("%-#{tabsigns}s \t%-#{bezahltsigns}i ", kunde.name, kunde.summe)
+      printf("\t%-#{bezahltsigns}i \t%i\n", kunde.bezahlt, kunde.nichtbezahlt)
     end
 
-    printf("%-#{tabsigns}s %i\n","Gesamt Summe", @gesamtsum)
+    printf("%-#{tabsigns}s \t%i\n", 'Gesamt Summe', @gesamtsum)
     print"\n"
   end
-
 end
 
 options = CommandLineParser.parse_command_line
 report = Report.new(options)
-kundenreport =report.generiere_bezahlReport(1,2)
-report.ausgabe(kundenreport,40,10)
+kundenreport = report.generiere_bezahl_report('Text', 'Price net cents')
+report.ausgabe(kundenreport, 40, 10)
